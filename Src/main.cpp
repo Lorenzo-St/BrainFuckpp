ec10c7704f3fad8d6eb9a68b7fb347dc8640368d
Lorenzo-St <chd_frost@yahoo.com>
2023-10-10 19:53:30 -0700

    Updated instruction definitions

--- Src/main.cpp
+++ Src/main.cpp
@@ -11,25 +11,25 @@
 // ----------------------------------------------------------------
 // has x amount of registers
 // commands are
-// > shift right by 1 memory space // Done
-//      note: cannot run off send of vector, maybe coult make it add registers
-// < shift left by 1 memory space // Done
-//      note: cannot run off start of vector, can be used to return to first register
-// + add 1 to current memory space // Done
-// - subtract 1 from current memory space // Done
-// , take input from keyboard // Done
-// . print to console // Done
-// [ start of loop // Done
-// ] if reg 0 is not zero, got back to start of loop, if not continue // Done
-// { spawn a thread with the passed instructions or function // Done
-// } end of thread
-// $ return statement, places the return in register 0 // Done
-// % thread return statement, places the current register's value in register 1 of the parent thread // Done
-// = function create next is name // Done
-//      note: a return value can be determined by placing the data pointer
-//            on a specified register, maybe even standardized as register 0
-// ( start of function // Done
-// ) close function // Done
+//  > shift right by 1 register
+//       note: cannot run off send of vector, maybe coult make it add registers
+//  < shift left by 1 register
+//       note: cannot run off start of vector, can be used to return to first register
+//  + add 1 to current register
+//  - subtract 1 from current register
+//  , take input from keyboard into current register
+//  . print register to console
+//  [ start of loop, if current register is 0, skip loop
+//  ] if current register is not zero, got back to start of loop, if not continue
+//  { spawn a thread with the passed instructions or function
+//  } end of thread
+//  $ return statement, places the return in register 0
+//  % thread return statement, places the current register's value in register 0 of the parent thread
+//  = function create next is name
+//       note: a return value can be determined by placing the data pointer
+//             on a specified register, maybe even standardized as register 0
+//  ( start of function
+//  ) close function
 // -----------------------------
 
 int main(int argc, char **argv)
